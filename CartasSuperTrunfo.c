5f858363b4096ac455991f1cdc214d4215261317
gabrielheberdf51-cpu <gabrielheberdf51@gmail.com>
2026-02-25 00:03:31 +0000

    Foi colocado exibição de dados variaveis e entradas de dados utilizando scanf printf no código do jogo de cartas Super Trunfo, para que o usuário possa interagir com o jogo e visualizar as informações das cartas durante a partida. As mudanças foram feitas no arquivo CartasSuperTrunfo.c, onde foram adicionados comandos para exibir os atributos das cartas e permitir que o jogador escolha qual atributo deseja comparar durante a jogada.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,41 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estado[3];
+  char codigo[4];
+  char nomeCidade[50];
+  int populacao;
+  float area;
+  float pib;
+  int pontosTuristicos;
   // Área para entrada de dados
+  printf("Digite a letra do estado (A-H): ");
+  scanf("%c", &estado);
 
-  // Área para exibição dos dados da cidade
+  printf("Digite o codigo da carta (ex; A01): ")
+  scanf("%s", codigo);
 
-return 0;
+  printf("Digite o nome da cidade: ");
+  scanf("%s", nomeCidade); // Nota: Para ler o nome da cidade com espaços, use fgets em vez de scanf
+  
+  printf("populacao: ");
+  scanf("%d", &populacao);
+
+  printf("area: ");
+  scanf("%f", &area);
+  
+  printf("PIB (em bilhões): ");
+  scanf("%f", &pib);
+
+  printf("Numeros de pontos turisticos: ");
+  scanf("%d", &pontosTuristicos);
+  // Exibição dos Dados
+  printf("\n--- Dados da carta ---\n");
+  printf("Estado: %c\n", estado);
+  printf("Codigo: %s\n", codigo);
+  printf("Nome da Cidade: %s\n", nomeCidade);
+  printf("População; %d\n", população);
+  printf("Área: %.2f km2\n", area);
+    
+  return 0;
 } 
